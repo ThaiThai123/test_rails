368ddb77c47a237079ef25878d8574a6a776720c
HGPMAC 169 <hgpmac169@Thes-MacBook-Air-98.local>
2019-10-29 17:52:20 -0700

    added new html files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get '/home_page2', to:'home#home_page2'
+  get '/home_page3', to:'home#home_page3'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
